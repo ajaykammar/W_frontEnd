5783dbfa5c20776dcd337b235d9a789920504c8b
ajay <Wefix40@gmail.com>
2025-03-26 07:06:47 +0530

    path updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import MissionVistion from '../components/Aboutus/MissionVistion'
-import Team from '../components/Aboutus/team'
-import CustomerReview from '../components/CustomerReview '
-import LogoSlider from '../components/Sliders/LogoSlider'
-import Statistics from '../components/Aboutus/Statistics'
-import ContactForm from '../components/ContactForm'
+import React from "react";
+import MissionVistion from "../components/Aboutus/MissionVistion";
+import Team from "../components/Aboutus/Team";
+import CustomerReview from "../components/CustomerReview ";
+import LogoSlider from "../components/Sliders/LogoSlider";
+import Statistics from "../components/Aboutus/Statistics";
+import ContactForm from "../components/ContactForm";
 
 const About = () => {
   return (
     <div>
-      <MissionVistion/>
-      <Statistics/>
-      <Team/>
-      <ContactForm/>
-      <CustomerReview/>
-      <LogoSlider/>
+      <MissionVistion />
+      <Statistics />
+      <Team />
+      <ContactForm />
+      <CustomerReview />
+      <LogoSlider />
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
